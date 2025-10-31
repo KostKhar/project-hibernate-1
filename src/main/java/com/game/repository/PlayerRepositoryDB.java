71e999cc7c08804361e0d5a34786304cc0d873c1
kostkhar <konstantin.kharik@onlinesup.com>
2025-10-31 23:14:37 +0300

    add sessionFactory

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,21 +1,30 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import com.mysql.cj.Session;
+import com.mysql.cj.xdevapi.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
+        String sql = " from players";
+
+
         return null;
     }
 
