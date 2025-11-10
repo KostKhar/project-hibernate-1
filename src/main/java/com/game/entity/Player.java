8d4c4d9123f37b5dee7649aa1dab7e1b55d24228
kostkhar <konstantin.kharik@onlinesup.com>
2025-11-10 10:10:29 +0300

    update getAll method

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
